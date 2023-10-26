15cfeeeb011e1db5d3861e9bc17831bc18e193f7
Lyudmila <lyudmila.kostornova@mail.ru>
2023-10-26 19:08:38 +0300

    Задание ко 2 семинару

---
+++ dz_1/Calculator.java
@@ -0,0 +1,8 @@
+public class Calculator {
+    public static double calculatingDiscount(double purchaseAmount, int
+    discountAmount) {
+    //	purchaseAmount -	сумма	покупки
+    //	discountAmount -	размер	скидки
+    return 0; // Метод должен возвращать сумму покупки со скидкой
+    }
+    }
