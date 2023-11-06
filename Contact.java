c480159620ddca218da385a8c87267d44a471f9d
Lyudmila <lyudmila.kostornova@mail.ru>
2023-11-06 14:14:01 +0300

    Задание к 5 семинару

---
+++ Contact.java
@@ -0,0 +1,17 @@
+public class Contact {
+// Для хранения информации (имя, номер телефона, электронная почта) о каждом контакте
+    private String name;
+    private String phoneNumber;
+    private String email;
+
+    public Contact(String name, String phoneNumber, String email) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Имя: " + name + ", Телефон: " + phoneNumber + ", Email: " + email;
+    }
+}
